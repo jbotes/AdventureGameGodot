aaf81814ff715b423e4f5503ded7cbfd54fcffa0
James Botes <jamesbotes@basensei.com>
2026-04-19 21:31:44 +0200

    JB_20260419_003
    check again

--- src/Game/Configs/bulletin_config.gd
+++ src/Game/Configs/bulletin_config.gd
@@ -1,11 +1,13 @@
 class_name BulletinConfig
 
 enum Keys {
-	InteractionPrompt
+	InteractionPrompt,
+	CraftingMenu
 }
 
 const BULLETIN_PATH := {
-	Keys.InteractionPrompt : "res://bulletins/interaction_prompt.tscn"
+	Keys.InteractionPrompt : "res://bulletins/interaction_prompt.tscn",
+	Keys.CraftingMenu : "res://bulletins/player_menus/crafting_menu.tscn"
 }
 
 # dont need to create a instance to use
